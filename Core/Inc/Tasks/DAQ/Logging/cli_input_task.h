19e58ce6a58a06ff21767427501ebc9e3938898f
Austin Tsai <aytsai@usc.edu>
2025-12-26 19:18:05 -0800

    print task states, io pins

--- Core/Inc/Tasks/DAQ/Logging/cli_input_task.h
+++ Core/Inc/Tasks/DAQ/Logging/cli_input_task.h
@@ -3,13 +3,27 @@
 
 #include "app.h"
 #include "watchdog_tasks_defs.h"
+#include "stm32f4xx_hal_gpio.h"
 #include <string.h>
 #include <stdio.h>
+#include <stdint.h>
 
 #define CLI_STACK_SIZE_WORDS 1024
 #define CLI_TASK_DELAY_MS 100
 #define CLI_TICKS_TO_WAIT 0
 #define CLI_BUFFER_SIZE 50
+
+#define NUM_PERIODIC_OUTPUTS 1
+#define IO_output_index 0
+
+#define PRINT_IO_PERIOD_MS 2000
+
+typedef struct {
+    char flag;
+    TickType_t last_tick_time;
+} cli_output_entry_t;
+
+cli_output_entry_t cli_output[NUM_PERIODIC_OUTPUTS];
 
 void cli_input_task(void *argument);
 void process_cmd(app_data_t *app, const char *cmd);
--- Core/Src/Tasks/DAQ/Logging/cli_input_task.c
+++ Core/Src/Tasks/DAQ/Logging/cli_input_task.c
@@ -2,11 +2,15 @@
 
 
 // Task: CLI Input
+static cli_output_entry_t cli_output[NUM_PERIODIC_OUTPUTS] = {0};
 
 void cli_input_task(void *argument) {
     app_data_t *data = (app_data_t *) argument;
     char cli_buffer[CLI_BUFFER_SIZE] = {0};
     uint8_t index = 0;
+
+    populate_array();
+
     for (;;) {
         TickType_t start = xTaskGetTickCount();
         uint8_t ch;
@@ -26,8 +30,34 @@ void cli_input_task(void *argument) {
                 }
             }
         }
+
+        print_toggled_outputs();
         xEventGroupSetBits(data->idwg_group, WD_CLI_INPUT);
         vTaskDelayUntil(&start, pdMS_TO_TICKS(CLI_TASK_DELAY_MS));
+    }
+}
+
+void populate_array() {
+    cli_output[IO_output_index].flag = 0;
+    cli_output[IO_output_index].last_tick_time = 0;
+}
+
+void print_toggled_outputs() {
+    TickType_t now = xTaskGetTickCount();
+    if (cli_output[IO_output_index].flag && (now - cli_output[IO_output_index].last_tick_time) >= pdMS_TO_TICKS(PRINT_IO_PERIOD_MS)) {
+        cli_output[IO_output_index].last_tick_time = now;
+
+        GPIO_PinState dpin1 = HAL_GPIO_ReadPin(GPIOA, PIN1);
+        GPIO_PinState dpin2 = HAL_GPIO_ReadPin(GPIOA, PIN2);
+        uint16_t a_value1 = adc_buffer[0];
+        uint16_t a_value2 = adc_buffer[1];
+        uint16_t a_value3 = adc_buffer[2];
+        uint16_t a_value4 = adc_buffer[3];
+        uint16_t a_value5 = adc_buffer[4];
+        uint16_t a_value6 = adc_buffer[5];
+        serial_print("ANO1: %u, ANO2: %u, ANO3: %u, ANO4: %u, ANO5: %u, ANO6: %u\r\n",
+                    a_value1, a_value2, a_value3, a_value4, a_value5, a_value6);
+        serial_print("DIN1: %d, DIN2: %d\r\n", dpin1, dpin2);
     }
 }
 
@@ -37,6 +67,11 @@ void process_cmd(app_data_t *app, const char *cmd) {
 
     if (strcmp(cmd, "h") == 0 || strcmp(cmd, "H") == 0) {
         send_task_help(app);
+        return;
+    }
+
+    if (strcmp(cmd, "t") == 0 || strcmp(cmd, "T") == 0) {
+        cli_output[IO_output_index].flag ^= 1;
         return;
     }
 
@@ -67,18 +102,23 @@ void start_stop_task(app_data_t *app, const char* task_name, int value) {
 }
 
 void send_task_help(app_data_t *app) {
-
-    char buf[128];
-
-    serial_print("Task names:\r\n{\r\n");
-
     for (size_t i = 0; i < NUM_TASKS; i++) {
-        snprintf(buf, sizeof(buf), "  %s\r\n", app->task_entries[i].name);
-        serial_print(buf);
+        eTaskState state = eTaskGetState(app->task_entries[i].handle);
+        const char *state_str;
+        switch (state) {
+            case eRunning:   state_str = "Running"; break;
+            case eReady:     state_str = "Ready"; break;
+            case eBlocked:   state_str = "Blocked"; break;
+            case eSuspended: state_str = "Suspended"; break;
+            case eDeleted:   state_str = "Deleted"; break;
+            default:         state_str = "Unknown"; break;
+        }
+        serial_print("%s: %s\r\n", app->task_entries[i].name, state_str);
     }
 
-    serial_print("}\r\nUse: <task_name>=1 to resume, <task_name>=0 to stop\r\n");
+    serial_print("\r\nUse: <task_name>=1 to resume, <task_name>=0 to stop\r\n");
     serial_print("Enter: h for help\r\n");
+    serial_print("Enter: t to toggle printing IO pins\r\n");
 }
 
 
